f60bebc09b5aae15970e2fe78803f02bcc6d29ea
max martinez <mmfern2003@gmail.com>
2023-10-18 18:39:19 -0400

    scraping Javascript code

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,14 @@
-// Intial setup
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var length = 12;
 
-// Setting up characters in the code.
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var number = "1234567890";
-var symbol = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-// Generating a password with random character using a for loop 
-function generatePassword() {
-  var allChars = upperCase + lowerCase + number + symbol;
-
-  var password = "";
-
-  for (var i = 0; i < length; i++) {
-    password += allChars[Math.floor(Math.random() * allChars.length)];
-  }
-  return password;
-}
-
-// Setting variable password to #password input
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   passwordText.value = password;
+
 }
 
-// Event listener for when user clicks on the button, a password generates
+// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-var generateBtn = document.querySelector("#generate");
