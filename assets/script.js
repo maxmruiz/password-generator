bb1046acc7aea9a0ed7e90c743a4e32e9c7e6b3c
max martinez <mmfern2003@gmail.com>
2023-10-18 18:11:13 -0400

    adding description comment to JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,14 @@
-// Assignment Code
+// Intial setup
 var generateBtn = document.querySelector("#generate");
 var length = 12;
 
+// Setting up characters in the code.
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var number = "1234567890";
 var symbol = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-
+// Generating a password with random character using a for loop 
 function generatePassword() {
   var allChars = upperCase + lowerCase + number + symbol;
 
@@ -19,13 +20,13 @@ function generatePassword() {
   return password;
 }
 
-// Write password to the #password input
+// Setting variable password to #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
-// Add event listener to generate button
+// Event listener for when user clicks on the button, a password generates
 generateBtn.addEventListener("click", writePassword);
 var generateBtn = document.querySelector("#generate");
