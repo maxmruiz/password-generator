f34800a6ab2062e22f95bd6d08c5827be29ff8b5
max martinez <mmfern2003@gmail.com>
2023-10-19 00:44:46 -0400

    describing the code so far with comments

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Character setup
 var lowerCase = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var upperCase = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var letters = [1,2,3,4,5,6,7,8,9,0];
@@ -9,9 +10,11 @@ var symbols = ["!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"];
 // Write password to the #password input
 function writePassword() {
 
+  // Combining all characters to one variable
   allChars = lowerCase + upperCase + letters + symbols;
   password = "";
 
+  // Itirating through each character and randomizing the outcome with Math.random
   for (var i = 0; i < length; i++) {
     password += allChars[Math.floor(Math.random() * allChars)];
     return password;
